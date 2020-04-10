7f16588f33be06c3439fccfb62f20a35d1ee1b3f
Abhishek Chaudhary <abhishek.chaudhary.in@gmail.com>
2020-04-11 04:45:10 +0530

    Initial Commit

---
+++ app/src/main/java/io/github/abhishek/emojify/MainActivity.java
@@ -0,0 +1,203 @@
+package io.github.abhishek.emojify;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+import androidx.databinding.DataBindingUtil;
+
+import java.io.File;
+import java.io.IOException;
+
+import io.github.abhishek.emojify.databinding.ActivityMainBinding;
+import timber.log.Timber;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_STORAGE_PERMISSION = 1;
+
+    private static final String FILE_PROVIDER_AUTHORITY = "com.example.android.fileprovider";
+
+    private String mTempPhotoPath;
+    private Bitmap mResultsBitmap;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        // Set up Timber
+        Timber.plant(new Timber.DebugTree());
+
+        binding.emojifyButton.setOnClickListener(v -> emojifyMe());
+        binding.saveButton.setOnClickListener(v -> saveMe());
+        binding.shareButton.setOnClickListener(v -> shareMe());
+    }
+
+    public void emojifyMe() {
+        // Check for the external storage permission
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // If you do not have permission, request it
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_STORAGE_PERMISSION);
+        } else {
+            // Launch the camera if the permission exists
+            launchCamera();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        // Called when you request permission to read and write to external storage
+        switch (requestCode) {
+            case REQUEST_STORAGE_PERMISSION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // If you get permission, launch the camera
+                    launchCamera();
+                } else {
+                    // If you do not get permission, show a Toast
+                    Toast.makeText(this, R.string.permission_denied, Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
+    }
+
+    /**
+     * Creates a temporary image file and captures a picture to store in it.
+     */
+    private void launchCamera() {
+
+        // Create the capture image intent
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the temporary File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = BitmapUtils.createTempImageFile(this);
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+
+                // Get the path of the temporary file
+                mTempPhotoPath = photoFile.getAbsolutePath();
+
+                // Get the content URI for the image file
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        FILE_PROVIDER_AUTHORITY,
+                        photoFile);
+
+                // Add the URI so the camera can store the image
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+                // Launch the camera activity
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // If the image capture activity was called and was successful
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // Process the image and set it to the TextView
+            processAndSetImage();
+        } else {
+
+            // Otherwise, delete the temporary image file
+            BitmapUtils.deleteImageFile(this, mTempPhotoPath);
+        }
+    }
+
+    /**
+     * Method for processing the captured image and setting it to the TextView.
+     */
+    private void processAndSetImage() {
+
+        // Toggle Visibility of the views
+        binding.emojifyButton.setVisibility(View.GONE);
+        binding.titleTextView.setVisibility(View.GONE);
+        binding.saveButton.setVisibility(View.VISIBLE);
+        binding.shareButton.setVisibility(View.VISIBLE);
+        binding.clearButton.setVisibility(View.VISIBLE);
+
+        // Resample the saved image to fit the ImageView
+        mResultsBitmap = BitmapUtils.resamplePic(this, mTempPhotoPath);
+
+
+        // Detect the faces and overlay the appropriate emoji
+        mResultsBitmap = Emojifier.detectFacesandOverlayEmoji(this, mResultsBitmap);
+
+        // Set the new bitmap to the ImageView
+        binding.imageView.setImageBitmap(mResultsBitmap);
+    }
+
+
+    /**
+     * OnClick method for the save button.
+     */
+    public void saveMe() {
+        // Delete the temporary image file
+        BitmapUtils.deleteImageFile(this, mTempPhotoPath);
+
+        // Save the image
+        BitmapUtils.saveImage(this, mResultsBitmap);
+    }
+
+    /**
+     * OnClick method for the share button, saves and shares the new bitmap.
+     */
+    public void shareMe() {
+        // Delete the temporary image file
+        BitmapUtils.deleteImageFile(this, mTempPhotoPath);
+
+        // Save the image
+        BitmapUtils.saveImage(this, mResultsBitmap);
+
+        // Share the image
+        BitmapUtils.shareImage(this, mTempPhotoPath);
+    }
+
+    /**
+     * OnClick for the clear button, resets the app to original state.
+     */
+    public void clearImage() {
+        // Clear the image and toggle the view visibility
+        binding.imageView.setImageResource(0);
+        binding.emojifyButton.setVisibility(View.VISIBLE);
+        binding.titleTextView.setVisibility(View.VISIBLE);
+        binding.saveButton.setVisibility(View.GONE);
+        binding.shareButton.setVisibility(View.GONE);
+        binding.clearButton.setVisibility(View.GONE);
+
+        // Delete the temporary image file
+        BitmapUtils.deleteImageFile(this, mTempPhotoPath);
+    }
+}
